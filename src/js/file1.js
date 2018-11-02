67ca99b0f5bf799a35e6cb98a8fa0e0671dc6227
Alexpolarlight <alexpolarlight@gmail.com>
2018-11-02 15:18:05 +0200

    Initial workflow setup

---
+++ src/js/file1.js
@@ -0,0 +1,7 @@
+//Consolo log 1
+console.log("This is file 1");
+
+//Consolo log 1
+alert("This is file 1");
+
+
---
+++ src/js/file2.js
@@ -0,0 +1,5 @@
+//Consolo log 2
+console.log("This is file 2");
+
+//Consolo log 2
+alert("This is file 2");
