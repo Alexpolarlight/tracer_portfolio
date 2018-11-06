5a992fa21c79a6b3d28bf838ee8392a95f5a51ad
Alexpolarlight <alexpolarlight@gmail.com>
2018-11-06 13:55:26 +0200

    Menu button, javascript & rotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,37 @@
-const gulp         = require('gulp');
-const browserSync  = require('browser-sync').create();
-const sass         = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src(["src/scss/*.scss"])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
 });
 
 // Copy All HTML files
-gulp.task('copyHtml', function(){
-    gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'));
-})
+gulp.task("copyHtml", function() {
+  gulp.src("src/*.html").pipe(gulp.dest("dist"));
+});
 
 // Optimize Images
-gulp.task('imageMin', () =>
-    gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+gulp.task("imageMin", () =>
+  gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"))
 );
 
 // // Minify JS
@@ -39,29 +42,29 @@ gulp.task('imageMin', () =>
 // })
 
 // Scripts
-gulp.task('scripts', function(){
-    gulp.src('src/js/*.js')
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+gulp.task("scripts", function() {
+  gulp
+    .src("src/js/*.js")
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
 });
 
 // Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: "./dist"  
-    });
-    gulp.watch(['src/js/*.js'], ['scripts',]);
-    gulp.watch(['src/scss/*.scss'], ['sass',]);
-    gulp.watch(['src/*.html'], ['copyHtml',]);
-    gulp.watch("dist/*.html").on('change', browserSync.reload);
+gulp.task("serve", ["sass"], function() {
+  browserSync.init({
+    server: "./dist"
+  });
+  gulp.watch(["src/js/*.js"], ["scripts"]);
+  gulp.watch(["src/scss/*.scss"], ["sass"]);
+  gulp.watch(["src/*.html"], ["copyHtml"]);
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['serve', 'copyHtml', 'message', 'imageMin', 'scripts']);
+gulp.task("default", ["serve", "copyHtml", "message", "imageMin", "scripts"]);
 
 // Logs Message
-gulp.task('message', function(){
-    return console.log('Gulp is running...');
-})
-
+gulp.task("message", function() {
+  return console.log("Gulp is running...");
+});
